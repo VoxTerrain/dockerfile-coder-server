4bc559d4701d0889859812a47e7842c1303dbd38
VoxTerrain <154274770+VoxTerrain@users.noreply.github.com>
2024-01-30 23:57:00 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-if [ -z "$PASSWORD" ]; then
-  echo "ERROR: PASSWORD environment variable is not set."
-  exit 1
-fi
-
-code-server --install-extension chrmarti.regex --auth password --password "VOX123"
+code-server --install-extension chrmarti.regex --auth none
